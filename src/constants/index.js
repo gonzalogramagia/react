564bc5a7b9d82402c3df8a9d47ef0369bbafd886
Gonzalo Gramaglia <gonzagramaglia@icloud.com>
2025-01-10 23:13:48 -0300

    Saas project

--- src/constants/index.js
+++ src/constants/index.js
@@ -215,6 +215,39 @@ const projects = [
     live_demo_link: "https://github.com/gonzagramaglia/pern-dashboard",
     source_code_link: "https://github.com/gonzagramaglia/pern-dashboard",
   },
+  {
+    name: "Chatbot [soon]",
+    description: `Entire, modern SaaS platform`,
+    tags: [
+      {
+        name: "typescript",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "next",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "trpc",
+        color: "green-text-gradient",
+      },
+      {
+        name: "prisma",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwindcss",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: underconstruction,
+    live_demo_link: "https://github.com/gonzagramaglia/saas-platform",
+    source_code_link: "https://github.com/gonzagramaglia/saas-platform",
+  },
 ];
 
 export { services, technologies, projects };
